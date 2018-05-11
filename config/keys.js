e1755f41c3b6ef08d5caa28259a42758891cc810
eazzyLEE <ohueisrael@yahoo.com>
2018-05-11 20:22:10 +0100

    second commit for pull

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://eazzyLEE:maths4lifejesus@ds117200.mlab.com:17200/devcircle"
+    "mongodb://eazzyLEE:maths4lifejesus@ds117200.mlab.com:17200/devcircle",
+  secretOrKey: "secret"
 };
