89be962ad13173ac9d43970e06e5530b606d2039
eazzyLEE <ohueisrael@yahoo.com>
2018-05-23 01:08:02 +0100

    Front-end Setup with React

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Landing from "./components/layouts/Landing";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
